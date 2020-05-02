c98a11bd0a8faea851124f99e8b24a9781a056fb
Paul Landázuri <5787964+LandazuriPaul@users.noreply.github.com>
2020-05-02 13:02:53 +0100

    feat: Adds socket.io events to the API

--- packages/api/src/modules/api.module.ts
+++ packages/api/src/modules/api.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 
-import { ConfigModule } from '../config/config.module';
+import { ConfigModule } from '~/config/config.module';
+
 import { AppModule } from './app/app.module';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [AppModule, ConfigModule],
+  imports: [AppModule, ConfigModule, EventsModule],
 })
 export class APIModule {}
