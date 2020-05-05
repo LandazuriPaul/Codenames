31c71e614fb3aba4a1918977b8a6747d6692eb1f
Paul Landázuri <5787964+LandazuriPaul@users.noreply.github.com>
2020-05-05 20:32:13 +0100

    script: Adds basic build_and_push script

--- scripts/build_and_push.sh
+++ scripts/build_and_push.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
-DOCKER_BUILDKIT=1 docker build -f ./packages/$PACKAGE/Dockerfile -t docker.pkg.github.com/landazuripaul/codenames/codenames-$PACKAGE:$VERSION .
+if [[ -z "${PACKAGE}" ]]; then
+  printf "You must specify a PACKAGE name (api or frontend) as an environment, e.g.: PACKAGE=api"
+  exit 1
+fi
 
-docker push docker.pkg.github.com/landazuripaul/codenames/codenames-$PACKAGE:$VERSION
+# TODO: read the version from the corresponding package.json
+LOCAL_VERSION="${VERSION:-latest}"
+
+DOCKER_IMAGE="docker.pkg.github.com/landazuripaul/codenames/codenames-$PACKAGE:$LOCAL_VERSION"
+
+printf "> Building the Docker image: $DOCKER_IMAGE ...\n"
+
+DOCKER_BUILDKIT=1 docker build -f ./packages/$PACKAGE/Dockerfile -t $DOCKER_IMAGE .
+
+printf "\n\n> Sending built image to the registry...\n"
+
+docker push $DOCKER_IMAGE
