855ab43f1d81388549613224572a76a358bf88bc
Paul Landázuri <5787964+LandazuriPaul@users.noreply.github.com>
2020-05-19 09:57:56 +0100

    [client] feat: footer after view port (scroll)

--- packages/client/src/components/Chat/chat.styles.tsx
+++ packages/client/src/components/Chat/chat.styles.tsx
@@ -2,7 +2,7 @@ import { styled } from '@material-ui/core/styles';
 import { Paper, Tabs } from '@material-ui/core';
 
 export const ChatContainer = styled(Paper)(({ theme }) => ({
-  height: `calc(100vh - ${theme.spacing(16)}px)`,
+  height: `calc(100vh - ${theme.spacing(8)}px)`,
   background: theme.palette.grey[300],
   display: 'flex',
   flexDirection: 'column',
--- packages/client/src/components/Layout/layout.styles.ts
+++ packages/client/src/components/Layout/layout.styles.ts
@@ -3,5 +3,5 @@ import { styled } from '@material-ui/core/styles';
 export const LayoutContainer = styled('div')(({ theme }) => ({
   width: '100%',
   flexGrow: 1,
-  height: `calc(100vh - ${theme.spacing(16)}px)`,
+  height: `calc(100vh - ${theme.spacing(8)}px)`,
 }));
