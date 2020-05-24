c335b7d6e9eabe1a993c900e90544d5b758174bb
Paul Landázuri <5787964+LandazuriPaul@users.noreply.github.com>
2020-05-24 11:20:22 +0100

    [client] fix: game settings valid only if at least 2 people in each team

--- packages/client/src/utils/isGameSettingsValid.ts
+++ packages/client/src/utils/isGameSettingsValid.ts
@@ -5,8 +5,8 @@ export function isGameSettingsValid(settings: GameSettings): boolean {
     settings.board.language &&
     settings.board.height > 2 &&
     settings.board.width > 2 &&
-    settings.teams.a.length > 0 &&
-    settings.teams.b.length > 0 &&
+    settings.teams.a.length > 1 &&
+    settings.teams.b.length > 1 &&
     settings.teams.spyA !== '' &&
     settings.teams.spyB !== ''
   );
