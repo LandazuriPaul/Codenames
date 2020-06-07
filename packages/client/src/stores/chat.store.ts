43696f0240ff72efbc7f305223bbc9cac893ffb6
Paul Landázuri <5787964+LandazuriPaul@users.noreply.github.com>
2020-06-07 17:13:24 +0100

    [client] feat: cleans chat store

--- packages/client/src/stores/chat.store.ts
+++ packages/client/src/stores/chat.store.ts
@@ -3,9 +3,7 @@ import { action, observable } from 'mobx';
 import {
   ChatEvent,
   GeneralChatEnvelope,
-  Team,
   TeamChatEnvelope,
-  Timestamp,
 } from '@codenames/domain';
 
 import { ChatMessage, GeneralChatMessage, TeamChatMessage } from '~/domain';
@@ -27,10 +25,8 @@ export class ChatStore extends SocketEmitterStore {
 
   @action
   init(): void {
-    // this.globalMessageList = [];
-    // this.teamMessageList = [];
-    this.generalChatMessageList = fakeGeneralMessageList;
-    this.teamChatMessageList = fakeTeamMessageList;
+    this.generalChatMessageList = [];
+    this.teamChatMessageList = [];
   }
 
   /*
@@ -80,59 +76,3 @@ export class ChatStore extends SocketEmitterStore {
     }
   }
 }
-
-// FIXME
-const fakeGeneralMessageList: GeneralChatMessage[] = [
-  {
-    type: 'expression',
-    message: {
-      team: Team.B,
-      text: 'Heeeey !',
-      timestamp: 1589867860806 as Timestamp,
-      username: 'Marcel',
-    },
-  },
-  {
-    type: 'information',
-    message: {
-      text: 'Bibi est arrivé !',
-      timestamp: 1589867871473 as Timestamp,
-    },
-  },
-  {
-    type: 'expression',
-    message: {
-      text: 'Je regarde comment vous jouez bande de nuls !',
-      timestamp: 1589867900032 as Timestamp,
-    },
-  },
-  {
-    type: 'expression',
-    isOwn: true,
-    message: {
-      team: Team.A,
-      text: 'Moi je peux pas voir votre petite convers',
-      timestamp: 1589867874019 as Timestamp,
-      username: 'Robert',
-    },
-  },
-];
-
-const fakeTeamMessageList: TeamChatMessage[] = [
-  {
-    type: 'expression',
-    message: {
-      text: 'Message privé !',
-      timestamp: 1589867860806 as Timestamp,
-      username: 'Marcel',
-    },
-  },
-
-  {
-    type: 'information',
-    message: {
-      text: 'Bibi est arrivé !',
-      timestamp: 1589867871473 as Timestamp,
-    },
-  },
-];
