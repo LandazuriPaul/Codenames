7271d394b87cabb2ed2232a68040c250b250a265
Paul Landázuri <5787964+LandazuriPaul@users.noreply.github.com>
2020-07-05 16:22:18 +0100

    [client] fix: Board entirely scrollable

--- packages/client/src/components/Board/board.styles.ts
+++ packages/client/src/components/Board/board.styles.ts
@@ -2,15 +2,8 @@ import { styled } from '@material-ui/core/styles';
 
 export const Container = styled('div')({
   width: '100%',
-  height: '100%',
+  minHeight: '100%',
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'center',
-  overflow: 'auto',
-
-  table: {
-    width: '100%',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-  },
 });
